7a984573244dd87109f9614f49a4b53b26408a01
YottaYocta <62713886+YottaYocta@users.noreply.github.com>
2021-11-10 22:53:24 -0600

    Update termed.rb

--- termed.rb
+++ termed.rb
@@ -1,12 +1,13 @@
 class Termed < Formula
+
   desc "A markdown note-taking program for the terminal"
   homepage "https://github.com/YottaYocta/Termed"
   url "https://github.com/YottaYocta/Termed/releases/download/v1.0.0/termed_MacOS.zip"
-  version "v1.0.0"
-  sha256 "42b90e80fbd3e83d10eedd7003b66090a15e7bf2"
+  sha256 "857efee3bdd7f547da2575abde50f665e27301ab88678bcfd28b78db4083e8e9"
   license "MIT"
 
   def install
     bin.install "termed"
   end
+
 end
