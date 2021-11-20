423860c6c19b0a85221634ffe9d7d98b44d22671
YottaYocta <chubbean.rq@gmail.com>
2021-11-19 23:28:09 -0600

    updated to release v1.1.3

--- TermME.rb
+++ TermME.rb
@@ -3,16 +3,16 @@ class Termme < Formula
   desc "A markdown note-taking program for the terminal"
   homepage "https://github.com/YottaYocta/TermME"
   license "MIT"
-  version "v1.1.2"
+  version "v1.1.3"
 
   if OS.mac?
     url "https://github.com/YottaYocta/TermME/releases/latest/download/TermME_MacOS.zip"
-    sha256 "17880e91bf01a4dfc35d4f61b38b4407b019a96a10c7249f249290ac06750f69"
+    sha256 "5aa9d85bb0d3909ae8eb58de38300047daaece3779cc73de24244f011e1da76d"
   end
 
   if OS.linux?
     url "https://github.com/YottaYocta/TermME/releases/latest/download/TermME_Ubuntu.zip"
-    sha256 "8600f2b3a88b6a8f25ec295ef7bb00caa6f86d482429e61a4f3b21c2c8f25e4b"
+    sha256 "40e346021e5f08fdf35b53bfbdc2643ccf50cb28124d0de515144f9368d708aa"
   end
 
   def install
