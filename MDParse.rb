27252445edb3f9731ca065bd0a23f2570d52f667
YottaYocta <chubbean.rq@gmail.com>
2021-12-01 11:01:35 -0600

    updated MDParse

--- MDParse.rb
+++ MDParse.rb
@@ -3,16 +3,16 @@ class Mdparse < Formula
   desc "A Markdown to CSV parser for exporting Markdown notes to various flashcard/study apps"
   homepage "https://github.com/YottaYocta/MDParse"
   license "MIT"
-  version "v0.0.1"
+  version "v0.0.2"
 
   if OS.mac?
     url "https://github.com/YottaYocta/MDParse/releases/latest/download/MDParse_MacOS.zip"
-    sha256 "024eb5ab9dfffa915440d9dc9e16e896e3a4945a72118ed283f3692a5ee85ae9"
+    sha256 "6414ebf2e1791d18b22bd6981b1e98a40410214ffdd05e3c1f718866aa68643a"
   end
 
   if OS.linux?
     url "https://github.com/YottaYocta/MDParse/releases/latest/download/MDParse_Ubuntu.zip"
-    sha256 "7bdd393444f3c2698ef5705685a96caf17c39c2b1d388c178f5623a9f364023f"
+    sha256 "dfc0b5754291a939eb909807f408b089b287f21823be44f52f4bdf001cc65598"
   end
 
   def install
